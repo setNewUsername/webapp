7a3d65e4e78aca121ab597b1c9728566082e4a36
n1k0lay <kolia.d97@gmail.com>
2022-06-29 23:54:52 +0500

    Админка: Структура

--- client/src/components/Admin/CreateProduct.jsx
+++ client/src/components/Admin/CreateProduct.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Button, Dropdown, Form, InputGroup, Modal} from "react-bootstrap";
+import {Context} from "../../index";
 
-const CreateProduct = () => {
+const CreateProduct = ({show, onHide}) => {
+
+    const {product} = useContext(Context);
+
     return (
-        <div>
-            
-        </div>
+        <Modal show={show}
+               fullscreen={true}
+               onHide={onHide}
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>Добавить новый тип</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                Добавление бренда
+                <Form>
+
+                   <Dropdown>
+                       <Dropdown.Toggle>Выберите жанр</Dropdown.Toggle>
+                       <Dropdown.Menu>
+                           {product.genres.map( genre =>
+                               <Dropdown.Item key={genre.id}>{genre.name}</Dropdown.Item>
+                           )}
+                       </Dropdown.Menu>
+                   </Dropdown>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant={"outline-danger"} onClick={onHide}>Закрыть</Button>
+                <Button variant={'success'} onClick={onHide}>Добавить</Button>
+            </Modal.Footer>
+        </Modal>
     );
 };
 
--- client/src/page/Admin.js
+++ client/src/page/Admin.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Container} from "react-bootstrap";
 import {observer} from "mobx-react-lite";
+import CreateGenre from "../components/Admin/CreateGenre";
+import CreateProduct from "../components/Admin/CreateProduct";
 
 const Admin = observer(() => {
+
+    const [genreVisible, setGenreVisible] = useState(false);
+    const [productVisible, setProductVisible] = useState(false);
+
     return (
         <Container className='d-flex flex-column pt-5'>
-            <Button variant='primary' className='mb-3'>Добавить товар</Button>
-            <Button variant='primary' className='mb-3'>Добавить товар</Button>
-            <Button variant='primary' className='mb-3'>Добавить товар</Button>
+            <Button
+                variant='primary'
+                className='mb-3'
+                onClick={() => setGenreVisible(true)}>
+                Добавить жанр
+            </Button>
+            <Button
+                variant='primary'
+                className='mb-3'
+                onClick={() => setProductVisible(true)}>
+                Добавить товар
+            </Button>
+            <CreateGenre show={genreVisible} onHide={() => setGenreVisible(false)}/>
+            <CreateProduct show={productVisible} onHide={() => setProductVisible(false)}/>
         </Container>
     );
 });
