6b8046b74cfce2d4bb3649fb3489b0d0acca07c1
n1k0lay <kolia.d97@gmail.com>
2022-06-30 10:40:42 +0500

    API: Настройка API авторизации

--- client/src/page/NotFound.jsx
+++ client/src/page/NotFound.jsx
@@ -2,9 +2,6 @@ import React, {useContext} from 'react';
 import {Context} from "../index";
 
 const NotFound = () => {
-
-    const {user} = useContext(Context);
-    console.log(user.isAuth)
     return (
         <div>
            Not found 404
