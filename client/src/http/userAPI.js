c6a3ca7de096450012cef8c68e55d966b851c0d5
n1k0lay <kolia.d97@gmail.com>
2022-06-30 10:40:42 +0500

    API: Настройка API авторизации

---
+++ client/src/http/userAPI.js
@@ -0,0 +1,15 @@
+import {$authHost, $host} from './index';
+
+export const registration = async (email,password) => {
+    debugger
+    const response = await $host.post('/api/user/registration', {email, password, access_rights: 'ADMIN'});
+    return response;
+}
+export const login = async (email,password) => {
+    const response = await $host.post('/api/user/login', {email, password});
+    return response;
+}
+export const check = async () => {
+    const response = await $host.post('/api/user/registration', );
+    return response;
+}
--- client/src/page/Auth.js
+++ client/src/page/Auth.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Card, Container, Form} from "react-bootstrap";
 import {NavLink, useLocation} from "react-router-dom";
 import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
+import {login, registration} from "../http/userAPI";
 
 const Auth = () => {
 
     const location = useLocation();
     const isLogin = location.pathname === LOGIN_ROUTE;
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmit = async () => {
+        if (isLogin) {
+            const response = await login(email, password);
+            console.log('login');
+            console.log(response);
+        } else {
+            const response = await registration(email, password);
+            debugger
+            console.log('Регистрация');
+            console.log(response);
+        }
+    }
 
 
     return (
@@ -15,15 +31,31 @@ const Auth = () => {
         >
             <Card style={{width: 600, padding: 20}}>
                 <h2 className='m-auto'>{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
-                <Form className='d-flex flex-column'>
+                <Form className='d-flex flex-column'
+                      onSubmit={(e) => {
+                          e.preventDefault();
+                          console.log('onSubmit');
+                          onSubmit();
+                      }}
+                >
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email адрес</Form.Label>
-                        <Form.Control type="email" placeholder="Введите email"/>
+                        <Form.Control
+                            type="email"
+                            placeholder="Введите email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Пароль</Form.Label>
-                        <Form.Control type="password" placeholder="Введите пароль"/>
+                        <Form.Control
+                            type="password"
+                            placeholder="Введите пароль"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
                     </Form.Group>
 
                     <Form.Group className="mb-3 d-flex justify-content-between align-items-center">
@@ -33,7 +65,10 @@ const Auth = () => {
                             : <Form.Text>Есть аккаунт? <NavLink
                                 to={LOGIN_ROUTE}>Войдите!</NavLink></Form.Text>
                         }
-                        <Button className='align-self-end' variant="primary" type="submit">
+                        <Button className='align-self-end'
+                                variant="primary"
+                                type="submit"
+                        >
                             {isLogin ? "Войти" : "Регистрация"}
                         </Button>
                     </Form.Group>
